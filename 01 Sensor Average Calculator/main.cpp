8e16e7e9811cb23a2c633749e5333368351b4acc
Elvia Sanchez Leiva <rosio.sanchezleiva@gmail.com>
2026-01-22 23:17:22 -0600

    Initialize temperature array and include headers
    Added necessary includes and initialized temperature array.

---
+++ 01 Sensor Average Calculator/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main () {
+  array <int,10> temperature;
+  int value;
+  for (size_t i {0}; i < temperature.size();i++){
+    cout << "Enter the temperature value: " << endl;
+    cin >> value;
+    temperature[i] = value;
+  }
+
+  for (
+
+  
