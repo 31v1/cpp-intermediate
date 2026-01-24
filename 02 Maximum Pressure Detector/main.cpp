c72f3d1993a2b2add106f679ada7e106d66b8ae0
Elvia Sanchez Leiva <rosio.sanchezleiva@gmail.com>
2026-01-23 20:16:22 -0600

    Fix formatting and include statements in main.cpp

--- 02 Maximum Pressure Detector/main.cpp
+++ 02 Maximum Pressure Detector/main.cpp
@@ -1,7 +1,25 @@
-# include <iostream>
-# include <array>
+# include<iostream>
+# include<array>
+
+using namespace std;
 
 int main () {
-  const size_t sizeArray{7}; 
+
+  const size_t sizeArray{7};
   array <int,sizeArray> maxPressures {580, 65, 987, 26, 31, 852, 78};
+  double pressureValue = maxPressures[0];
+  int maxIndex = 0;
+  
+  for (size_t i {0}; i < maxPressures.size(); i++){
+    if (pressureValue < maxPressures[i]){
+        pressureValue = maxPressures[i];
+        maxIndex=i;
+        }
+  }
+  cout <<"The maximum pressure is: "<< pressureValue 
+  << " on index: " << maxIndex << endl;
+
+return 0;
+}
+    
   
